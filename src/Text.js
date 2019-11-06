b40f56f0e8290da487e499d750fed9de61343219
Marcin Kubacki <neck12@interia.pl>
2019-11-06 15:26:27 +0100

    First commit

---
+++ src/Text.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Text.css';
+
+const Text = () => {
+  return (
+    <p className='red'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla perferendis ea tempore suscipit et, aliquid animi soluta nostrum facilis deserunt eveniet illo quidem, vitae accusamus nisi. Illo voluptate commodi iure!</p>
+  );
+}
+
+export default Text;
