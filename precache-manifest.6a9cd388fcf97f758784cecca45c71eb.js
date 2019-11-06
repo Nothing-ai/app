dbe8f19f1a75c34672ee5a1ae36413dfc384517c
Marcin Kubacki <neck12@interia.pl>
2019-11-06 16:03:03 +0100

    Updates

---
+++ precache-manifest.6a9cd388fcf97f758784cecca45c71eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1d4be37dad87a309de71e47a6f18931",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "919d8c93d25fb3425ad6",
+    "url": "/app/static/css/main.922a9fef.chunk.css"
+  },
+  {
+    "revision": "0239938de181e75c50bb",
+    "url": "/app/static/js/2.ea2fb4f6.chunk.js"
+  },
+  {
+    "revision": "919d8c93d25fb3425ad6",
+    "url": "/app/static/js/main.6137b0ab.chunk.js"
+  },
+  {
+    "revision": "8489f3f6c2dacc320bca",
+    "url": "/app/static/js/runtime-main.9b318e0b.js"
+  }
+]);
